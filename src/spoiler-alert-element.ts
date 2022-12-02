3ad28c793babc783a6914b5c84e97ea0887cf295
Keith Cirkel <keithamus@users.noreply.github.com>
2022-12-02 17:25:47 +0000

    initial implementation

---
+++ src/spoiler-alert-element.ts
@@ -0,0 +1,62 @@
+const html = String.raw
+
+/**
+ * Mask text until the user interacts with it!
+ *
+ * ```
+ * <spoiler-alert>Hidden text</spoiler-alert>
+ * ```
+ */
+class SpoilerAlertElement extends HTMLElement {
+  static observedAttributes = ['spoiled']
+  #renderRoot = this.attachShadow({mode: 'open'})
+
+  connectedCallback(): void {
+    // eslint-disable-next-line github/no-inner-html
+    this.#renderRoot.innerHTML = html`
+      <style>
+        :host(:not([spoiled])) {
+          background: black;
+        }
+        :host(:not([spoiled])) span {
+          visibility: hidden;
+        }
+      </style>
+      <span>
+        <slot></slot>
+      </span>
+    `
+    this.addEventListener('click', this)
+  }
+
+  get spoiled(): boolean {
+    return this.hasAttribute('spoiled')
+  }
+
+  set spoiled(value: boolean) {
+    this.toggleAttribute('spoiled', value)
+  }
+
+  handleEvent(event: Event) {
+    this.spoiled = true
+    try {
+      getSelection()?.removeAllRanges()
+    } catch {
+      // Empty block!
+    }
+  }
+
+}
+
+declare global {
+  interface Window {
+    SpoilerAlertElement: typeof SpoilerAlertElement
+  }
+}
+
+export default SpoilerAlertElement
+
+if (!window.customElements.get('spoiler-alert')) {
+  window.SpoilerAlertElement = SpoilerAlertElement
+  window.customElements.define('spoiler-alert', SpoilerAlertElement)
+}
--- test/test.ts
+++ test/test.ts
@@ -1,26 +1,26 @@
 import {assert, fixture, html} from '@open-wc/testing'
-import '../src/custom-element'
+import '../src/spoiler-alert-element'
 
-describe('custom-element', function () {
+describe('spoiler-alert', function () {
   describe('element creation', function () {
     it('creates from document.createElement', function () {
-      const el = document.createElement('custom-element')
-      assert.equal('CUSTOM-ELEMENT', el.nodeName)
+      const el = document.createElement('spoiler-alert')
+      assert.equal('SPOILER-ALERT', el.nodeName)
     })
 
     it('creates from constructor', function () {
-      const el = new window.CustomElementElement()
-      assert.equal('CUSTOM-ELEMENT', el.nodeName)
+      const el = new window.SpoilerAlertElement()
+      assert.equal('SPOILER-ALERT', el.nodeName)
     })
   })
 
   describe('after tree insertion', function () {
     beforeEach(async function () {
-      await fixture(html` <custom-element></custom-element>`)
+      await fixture(html` <spoiler-alert></spoiler-alert>`)
     })
 
     it('initiates', function () {
-      const ce = document.querySelector('custom-element')
+      const ce = document.querySelector('spoiler-alert')
       assert.equal(ce?.textContent, ':wave:')
     })
   })
